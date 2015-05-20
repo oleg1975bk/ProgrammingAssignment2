535aedfcf0cf2a604752713bd8eceb37ee944696
Oleg <oleg1975@bk.ru>
2015-05-20 23:08:57 +0300

    Ura

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  inverse_matrix<-NULL
+  set<-function(Matrix){
+    x<<-Matrix
+    inverse_matrix<<-NULL
+  }
+  get<-function() x
+  setinverse<-function(inver) inverse_matrix<<-inver
+  getinverse<-function() inverse_matrix
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 
 }
 
@@ -12,4 +21,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse_matrix<-x$getinverse()
+  if(!is.null(inverse_matrix)){
+    message("From cashe")
+    return(inverse_matrix)
+  }
+  matrix<-x$get()
+  inverse_matrix<-solve(matrix,...)
+  x$setinverse(inverse_matrix)
+  inverse_matrix
 }
